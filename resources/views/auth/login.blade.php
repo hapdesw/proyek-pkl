3bdfb6e4dae35ab11b17d3b9d80e262c3f0c805b
hapdesw <happydesitawidyantari@gmail.com>
2025-01-11 20:28:35 +0700

    Mengubah tampilan login beserta background

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,12 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700&display=swap" rel="stylesheet">
     <style>
         body {
-            background-image: url('/img/bmkg_6.jpg'); /* Path ke gambar */
+            background-image: url('/img/bg.jpg'); /* Path ke gambar */
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
+            font-family: 'Plus Jakarta Sans', sans-serif; /* Set font default */
         }
 
         .bg-overlay {
@@ -26,14 +28,13 @@
         <div class="absolute inset-0 bg-overlay"></div>
 
         <!-- Login -->
-        <div class="relative flex flex-col items-center justify-center px-6 py-6 mx-auto md:h-screen lg:py-0">
-          
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-20 sm:max-w-md xl:p-0 dark:bg-gray-800/80 dark:border-gray-700">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-slate-900 md:text-2xl dark:text-white" >
-                        Sistem Layanan Data dan Informasi
-                    </h1>
-                    <form class="space-y-4 md:space-y-6"  method="POST">
+        <div class="relative flex flex-col items-center justify-center h-full px-6 py-6">
+            <div class="w-full max-w-sm bg-white rounded-lg shadow dark:border dark:bg-gray-800/80 dark:border-gray-700">
+                <h1 class="text-xl pt-6 pb-0 font-bold leading-tight tracking-tight text-slate-600 md:text-xl dark:text-white text-center">
+                    Sistem Layanan Data dan Informasi
+                </h1>
+                <div class="p-4 space-y-2 md:space-y-6 sm:p-8">
+                    <form class="space-y-2 md:space-y-6" method="POST">
                         @csrf
                         <div>
                             <label for="username" class="block mb-2 text-sm font-medium text-black dark:text-white">Username</label>
@@ -43,15 +44,11 @@
                             <label for="password" class="block mb-2 text-sm font-medium text-black dark:text-white">Password</label>
                             <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                         </div>
-                        <div class="flex items-center justify-between">
-                            <div class="flex items-start">
-                            </div>
-                        </div>
-                        <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-800 ease-out duration-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Log in</button> 
+                        <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-800 ease-out duration-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Log in</button> 
                     </form>
                 </div>
             </div>
         </div>
-      </section>
+    </section>
 </body>
 </html>
