8980471f7a11f54ce68d386c4f6d3f0a77e76203
hapdesw <happydesitawidyantari@gmail.com>
2025-01-12 15:52:22 +0700

    Menambahkan breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Petugas\PetugasController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
 
+Route::middleware(['auth', 'Petugas_1000'])->group(function () {
+    Route::get('/dashboard', [PetugasController::class, 'dashboard'])->name('dashboard'); 
+}); 
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
